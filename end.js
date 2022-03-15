f8d836838da8557157033cedf7fd2a3dd846b34c
ElsieMay <elsiemaylawrie1@gmail.com>
2022-03-15 15:50:28 +1100

    added highScore files

--- end.js
+++ end.js
@@ -15,6 +15,15 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
     e.preventDefault()
     const score = {
-        score
+        score: mostRecentScore,
+        name: username.value;
     }
+    highScores.push(score);
+    highScores.sort((a,b)=>{
+        return b.score - a.score
+    });
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores))
+    window.location.assign('/');
 }
