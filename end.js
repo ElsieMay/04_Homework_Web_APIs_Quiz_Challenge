0b0905a15ec42f4c79682dc749adf8468098b40a
ElsieMay <elsiemaylawrie1@gmail.com>
2022-03-15 15:44:34 +1100

    added end page css and js

---
+++ end.js
@@ -0,0 +1,20 @@
+const username = document.querySelector("#username");
+const saveScoreBtn = document.querySelector("#saveScoreBtn");
+const finalScore = document.querySelector("#finalScore");
+const mostRecentScore = document.querySelector("#mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) ||
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+    saveScoreBtn.disabled = !username.value
+});
+
+saveHighScore = e => {
+    e.preventDefault()
+    const score = {
+        score
+    }
+}
