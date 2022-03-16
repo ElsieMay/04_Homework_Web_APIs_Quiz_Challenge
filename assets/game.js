ae398f2e35bb6e3742f971911f11ecc77fb511b5
ElsieMay <elsiemaylawrie1@gmail.com>
2022-03-16 21:23:05 +1100

    added ./ to js

--- assets/game.js
+++ assets/game.js
@@ -76,7 +76,7 @@ function startTimer() {
 		if (timerCount === 0) {
 			clearInterval(timer);
 			//Takes user to End Page//
-			return window.location.assign("/end.html");
+			return window.location.assign("./end.html");
 		}
 	}, 1000);
 }
@@ -85,7 +85,7 @@ getNewQuestion = () => {
 	if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
 		localStorage.setItem("mostRecentScore", score);
 		//Takes user to End Page//
-		return window.location.assign("/end.html");
+		return window.location.assign("./end.html");
 	}
 	questionCounter++;
 	progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
